1b97cb964dad354331392c98916a4c3bb1d9d381
Tom Patton <tom.patton22@gmail.com>
2018-09-22 07:32:37 -0400

    form updates and javascript for desktop

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,3 +2,18 @@ import "bootstrap";
 
 import { ctaScroller } from '../components/homepage';
 ctaScroller();
+
+import { hmeScroller } from '../components/homepage';
+hmeScroller();
+
+import { wiaScroller } from '../components/homepage';
+wiaScroller();
+
+import { widScroller } from '../components/homepage';
+widScroller();
+
+import { mwScroller } from '../components/homepage';
+mwScroller();
+
+import { ctScroller } from '../components/homepage';
+ctScroller();
