b016f27aaaa1d77d1cb645585d2722c85de888b8
Tom Patton <tom.patton22@gmail.com>
2019-02-13 22:32:24 -0500

    Validations work

--- app/controllers/inbounds_controller.rb
+++ app/controllers/inbounds_controller.rb
@@ -4,7 +4,18 @@ class InboundsController < ApplicationController
   end
   def create
     @inbound = Inbound.create(inbound_params)
+
+    if @inbound.save
+      flash[:notice] = "Update Successfull!"
+      redirect_to :root
+    else
+      flash[:notice] = "Unable to make update"
+      redirect_to :root
+    end
   end
+
+  private
+
   def inbound_params
     params.require(:inbound).permit(:name, :email, :message)
   end
