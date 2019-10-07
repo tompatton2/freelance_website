58644fd92fc68c276e1b64386eee94b1fc66cb99
Tom Patton <30573307+tompatton2@users.noreply.github.com>
2019-10-06 18:12:38 -0700

    [BW-2] Black and White Page Setup (#37)
    * [BW-2] Black and White Setup
    
    * route fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   get 'home', to: 'pages#home', as: 'home'
-
   get "pages/download_pdf"
-
   get 'resume', to: 'pages#resume', as: 'resume'
+
+  get 'black_and_white_landing', to: 'black_and_white#landing'
 
   resources :inbounds, only: [:create, :new]
 end
